fd00ddbad233468de4715381246535ddf2a3740d
Lorena Villamizar <lorenavillamizarflorez32@gmail.com>
2022-07-23 19:18:52 -0500

    creación del submenu,introducción practica con JavaScript

--- 01-restaurante/src/app/app.component.ts
+++ 01-restaurante/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'proyecto1';
+  ngAfterViewInit():void{
+   const btnRed= document.querySelector('#red');
+   const btnBlue=document.querySelector('#blue');
+   const btnGreen=document.querySelector('#green');
+   const textToChange:HTMLElement= document.querySelector('#acciones p')!;
+   btnRed?.addEventListener('click',()=>{
+     textToChange.style.backgroundColor='red';
+   })
+   btnBlue?.addEventListener('click',()=>{
+    textToChange.style.backgroundColor='blue'
+   })
+   btnGreen?.addEventListener('click',()=>{
+    textToChange.style.backgroundColor='green';
+   })
+    
+  }
 }
